492634b12e718f3b48a25549842b422bcb84dd59
familivichgarri <familivichgarri@gmail.com>
2024-07-15 17:58:02 +0300

    fix

---
+++ src/main/java/itstep/learning/App.java
@@ -0,0 +1,15 @@
+package itstep.learning;
+
+import itstep.learning.base.Begin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        Begin program = new Begin();
+        program.menu();
+    }
+}
